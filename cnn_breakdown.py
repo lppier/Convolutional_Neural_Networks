e97f203bfdb0319b8dacf075e781af31e038496b
Pier <madstrum@gmail.com>
2018-03-08 14:41:49 +0800

    CNN filter demo

--- cnn_breakdown.py
+++ cnn_breakdown.py
@@ -1,9 +1,13 @@
+# This code demonstrates the role of filters in convolutional neural networks
+# Two filters are created, a horizontal line filter and a vertical line filter of 7 x 7 pixels each
+
 
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_sample_images
 from PIL import Image
+
 
 def plot_image(image):
     plt.imshow(image, cmap="gray", interpolation="nearest")
@@ -12,16 +16,42 @@ def plot_image(image):
 
 
 def plot_color_image(image):
-    plt.imshow(image.astype(np.uint8),interpolation="nearest")
+    plt.imshow(image.astype(np.uint8), interpolation="nearest")
     plt.axis("off")
     plt.show()
+
 
 # Load sample images
 sample_imgs = load_sample_images().images
 dataset = np.array(sample_imgs, dtype=np.float32)
 batch_size, height, width, channels = dataset.shape
-plot_color_image(dataset[0,:,:,:])
-plot_color_image(dataset[1,:,:,:])
+plot_color_image(dataset[0, :, :, :])
+plot_color_image(dataset[1, :, :, :])
 
+# Create 2 filters
+filters_test = np.zeros(shape=(7, 7, channels, 2))
 
+# Create horizontal line filter [row, col, channels, idx]
+filters_test[:, 3, :, 0] = 1
 
+# Create vertical line filter [row, col, channels, idx]
+filters_test[3, :, :, 1] = 1
+
+X = tf.placeholder(tf.float32, shape=(None, height, width, channels))
+convolution = tf.nn.conv2d(X, filters_test, strides=[1, 2, 2, 1], padding="SAME")
+
+# output [batch_size, height, width, filter_idx]
+with tf.Session() as sess:
+    output = sess.run(convolution, feed_dict={X: dataset})
+
+# Show the flower 1st and 2nd feature map
+plt.imshow(output[0, :, :, 0], cmap="gray")
+plt.show()
+plt.imshow(output[0, :, :, 1], cmap="gray")
+plt.show()
+
+# Show the temple 1st and 2nd feature map
+plt.imshow(output[1, :, :, 0], cmap="gray")
+plt.show()
+plt.imshow(output[1, :, :, 1], cmap="gray")
+plt.show()
